1aef3480e05259525a8a305ba71a01fcbf90bdb0
Jhyden Ellana <jhydenellana@gmail.com>
2023-11-30 09:26:39 +0800

    Info Block update - Add sections for "Default Value", "Examples" & "Parameters"

--- src/pages/docs/components/infoBlock.vue
+++ src/pages/docs/components/infoBlock.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="grid gap-2 px-3 py-2">
     <div class="flex items-center gap-2 text-xl text-gray-500">
-      <span class="rounded-md bg-teal-700 px-2 font-semibold text-light">
+      <span
+        class="rounded-md bg-teal-700 px-2 font-semibold leading-tight text-light"
+      >
         {{ name }}
       </span>
       <span class="font-black">:</span>
@@ -9,7 +11,52 @@
     </div>
     <div class="">
       <div class="text-xl font-bold text-gray-500">Description</div>
-      <div class="">{{ description }}</div>
+      <div class="px-2" v-html="description"></div>
+    </div>
+    <div v-if="!!defaultVal" class="">
+      <div class="text-xl font-bold text-gray-500">Default Value</div>
+      <div class="flex flex-wrap gap-2 px-2">
+        <span
+          class="rounded-md border border-foreground bg-teal-700/25 px-2 py-0.5 font-semibold leading-tight"
+          v-html="defaultVal"
+        />
+      </div>
+    </div>
+    <div v-if="examples.length > 0" class="">
+      <div class="text-xl font-bold text-gray-500">Examples</div>
+      <div class="flex flex-wrap gap-2 px-2">
+        <template v-for="ex in examples" :key="ex">
+          <span
+            class="rounded-md border border-foreground bg-teal-700/25 px-2 py-0.5 font-semibold leading-tight"
+            v-html="ex"
+          />
+        </template>
+      </div>
+    </div>
+
+    <div v-if="params.length > 0" class="">
+      <div class="text-xl font-bold text-gray-500">Parameters</div>
+      <div class="grid gap-2 px-2">
+        <template v-for="param in params" :key="param">
+          <div
+            class="rounded-md border border-foreground/25 bg-teal-700/5 px-3 py-2"
+          >
+            <div class="flex items-center gap-2 text-xl text-gray-500">
+              <span
+                class="rounded-md bg-amber-700 px-2 font-semibold leading-tight text-light"
+              >
+                {{ param.name }}
+              </span>
+              <span class="font-black">:</span>
+              <span class="font-bold"> {{ param.type }} </span>
+            </div>
+            <div class="">
+              <div class="text-xl font-bold text-gray-500">Description</div>
+              <div class="px-2" v-html="param.description"></div>
+            </div>
+          </div>
+        </template>
+      </div>
     </div>
   </div>
 </template>
@@ -22,6 +69,18 @@ const props = defineProps({
   description: {
     type: String,
     default: "",
+  },
+  defaultVal: {
+    type: String,
+    default: null,
+  },
+  examples: {
+    type: Array,
+    default: [],
+  },
+  params: {
+    type: Array,
+    default: [],
   },
 });
 </script>
