a631bcc30909f0c5748b8fc377ddd94f6a6d75cc
Jhyden Ellana <jhydenellana@gmail.com>
2023-11-30 09:25:35 +0800

    Prepare Docs for Input Field

--- src/pages/docs/components/input/index.vue
+++ src/pages/docs/components/input/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <div></div>
+  <PageBlock title="Input" :properties="compProps"></PageBlock>
 </template>
+
+<script setup>
+import { computed, defineAsyncComponent, ref } from "vue";
+import compProps from "./props.json";
+
+const PageBlock = defineAsyncComponent(() => import("../pageBlock.vue"));
+</script>
