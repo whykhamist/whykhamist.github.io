65fd347ac40de0c438fe4cab7ed617208dc15533
Jhyden Ellana <jhydenellana@gmail.com>
2023-12-13 13:58:53 +0800

    Service Worker Register update - Add auto update every 15 minutes

--- src/plugins/swRegister.js
+++ src/plugins/swRegister.js
@@ -4,6 +4,7 @@ import { registerSW } from "virtual:pwa-register";
 
 export default {
   install: async (app) => {
+    const intervalMS = 15 * 60 * 1000;
     const updateSW = ref(
       registerSW({
         onOfflineReady: () => {
@@ -12,10 +13,27 @@ export default {
           system.settings.pwa.offline.confirm = false;
         },
         onNeedRefresh: () => {},
+        onRegisteredSW(swUrl, r) {
+          r &&
+            setInterval(async () => {
+              if (!(!r.installing && navigator)) return;
+
+              if ("connection" in navigator && !navigator.onLine) return;
+
+              const resp = await fetch(swUrl, {
+                cache: "no-store",
+                headers: {
+                  cache: "no-store",
+                  "cache-control": "no-cache",
+                },
+              });
+
+              if (resp?.status === 200) await r.update();
+            }, intervalMS);
+        },
       })
     );
 
     await updateSW.value();
-    app.provide("updateSW", updateSW);
   },
 };
