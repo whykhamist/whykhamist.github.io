fc87d7d1e03f3c142de6b2f43ca604dc2ece2c49
Jhyden Ellana <jhydenellana@gmail.com>
2023-12-18 12:49:08 +0800

    New docs layout utilizing new Sidebar

---
+++ src/layouts/docs.vue
@@ -0,0 +1,169 @@
+<template>
+  <LSLayout
+    class="!relative"
+    :style="{
+      paddingTop: systemStore.settings.navbar.fixed
+        ? `${headerSize.height ?? 0}px`
+        : '',
+    }"
+    ref="mainLayout"
+  >
+    <TopNav :layoutRef="mainLayout" @resize="(size) => (headerSize = size)">
+      <template #prepend>
+        <TButton
+          v-if="!$xl"
+          icon="menu"
+          class="aspect-square w-10 rounded-full"
+          @click="sideBar.show = true"
+        />
+      </template>
+      <template #append>
+        <TButton
+          v-if="!$lg"
+          icon="folder"
+          class="aspect-square w-10 rounded-full"
+          @click="rtlSideBar.show = true"
+        />
+      </template>
+    </TopNav>
+    <PageContainer
+      class="mx-auto w-full !max-w-[2500px] justify-center"
+      style=""
+    >
+      <SideBar
+        :headerSize="headerSize"
+        contentClass="border-r border-foreground/25"
+        :fixed="!$xl"
+        :show="sideBar.show"
+        :menu="menu"
+        @close="sideBar.show = false"
+      />
+
+      <router-view v-slot="{ Component }">
+        <transition
+          enter-from-class="opacity-0"
+          leave-to-class="opacity-0"
+          enter-active-class="transition duration-300 delay-300"
+          leave-active-class="transition duration-300"
+        >
+          <component :is="Component" class="w-full xl:max-w-7xl"> </component>
+        </transition>
+      </router-view>
+
+      <RtlSideBar
+        :headerSize="headerSize"
+        wrapperClass="border-r border-foreground/25"
+        :fixed="!$lg"
+        :show="rtlSideBar.show"
+        rtl
+        @close="rtlSideBar.show = false"
+      />
+    </PageContainer>
+    <TScrollUp
+      class="bottom-5 right-8 z-50 rounded-2xl bg-gray-900/75 px-5 py-2 text-gray-100 shadow-md dark:bg-gray-500/75"
+    >
+      <TIcon name="keyboard_double_arrow_up" size="sm" />
+    </TScrollUp>
+    <Footer
+      class="flex h-32 items-end justify-end border-t border-foreground/25 bg-gray-300 dark:bg-gray-700"
+    >
+      <div class="px-5 py-2 text-sm text-gray-400">
+        <b>Prepared By:</b> Jhyden Jhoe Marx M. Ellana
+      </div>
+    </Footer>
+  </LSLayout>
+</template>
+
+<script setup>
+import {
+  computed,
+  defineAsyncComponent,
+  onBeforeUnmount,
+  onMounted,
+  ref,
+  watch,
+} from "vue";
+import { useSystemStore } from "@/stores";
+import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
+
+const LSLayout = defineAsyncComponent(() => import("./lockScreenLayout.vue"));
+const TopNav = defineAsyncComponent(() => import("./topNav.vue"));
+const SideBar = defineAsyncComponent(() => import("./sidebar/main.vue"));
+const RtlSideBar = defineAsyncComponent(() => import("./sidebar/rMain.vue"));
+
+const systemStore = useSystemStore();
+
+const $screen = useBreakpoints(breakpointsTailwind);
+const $lg = $screen.greaterOrEqual("lg");
+const $xl = $screen.greaterOrEqual("xl");
+
+const mainLayout = ref(null);
+const headerSize = ref({});
+const sideBar = ref({
+  show: false,
+});
+const rtlSideBar = ref({
+  show: false,
+});
+const menu = ref([
+  {
+    title: "Main Navigation",
+    hidden: false,
+    links: [
+      {
+        label: "Components",
+        sub: [
+          {
+            label: "Button",
+            to: { name: "button" },
+          },
+          {
+            label: "Collapse",
+            to: { name: "collapse" },
+          },
+          {
+            label: "Forms",
+            sub: [
+              {
+                label: "Field Wrapper",
+                to: { name: "fieldwrapper" },
+              },
+              {
+                label: "Input",
+                to: { name: "input" },
+              },
+            ],
+          },
+          {
+            label: "Icons",
+            to: { name: "icon" },
+          },
+          {
+            label: "Size Observer",
+            to: { name: "sizeobserver" },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Others",
+    hidden: false,
+    links: [
+      {
+        label: "Unauthorized",
+        to: { name: "401" },
+        icon: null,
+      },
+    ],
+  },
+]);
+
+watch($xl, () => {
+  sideBar.value.show = false;
+});
+
+watch($lg, () => {
+  rtlSideBar.value.show = false;
+});
+</script>
